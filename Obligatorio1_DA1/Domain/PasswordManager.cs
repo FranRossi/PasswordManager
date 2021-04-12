db7eb53323beff19e2ca0445a49dbaa145635245
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-04-11 21:03:47 -0300

    Add login tests

--- Obligatorio1_DA1/Domain/PasswordManager.cs
+++ Obligatorio1_DA1/Domain/PasswordManager.cs
@@ -1,15 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Obligatorio1_DA1
 {
     public class PasswordManager
     {
+        private List<User> userList;
+
+        public PasswordManager()
+        {
+            userList = new List<User>();
+        }
+
         public void createUser(string name, string pass)
         {
             if (pass.Length < 5)
                 throw new ArgumentException("The password is too short (min. 5 characters).");
             else if (pass.Length > 25)
                 throw new ArgumentException("The password is too long (max. 25 characters).");
+
+            if (userList.Exists(user => user.Name == name))
+                throw new ArgumentException("The username is already taken");
+
+            User newUser = new User(name, pass);
+            userList.Add(newUser);
+        }
+
+        public Boolean login(string name, string pass)
+        {
+            foreach (User user in userList)
+                if (user.Name == name) 
+                    if (user.Pass == pass)
+                        return true;
+                    else
+                        return false;
+            return false;
         }
     }
 }
---
+++ UnitTestObligatorio1/UnitTestSignUp.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obligatorio1_DA1;
+using System;
+
+namespace UnitTestObligatorio1
+{
+    [TestClass]
+    public class UnitTestSignUp
+    {
+        
+        [TestMethod]
+        public void createPasswordManager()
+        {
+            PasswordManager passwordManager = new PasswordManager();
+            Assert.IsNotNull(passwordManager);
+        }
+
+
+        PasswordManager passwordManager;
+        [TestInitialize]
+        public void createPasswordManagerBeforeTests()
+        {
+            passwordManager = new PasswordManager();
+        }
+
+        [TestMethod]
+        public void createValidUser()
+        {
+            try
+            {
+                passwordManager.createUser("Juan", "hola123");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "The password is too short (min. 5 characters).")]
+        public void createInvalidUserPasswordTooShort()
+        {
+            passwordManager.createUser("Juan", "hola");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "The password is too long (max. 25 characters).")]
+        public void createInvalidUserPasswordTooLong()
+        {
+            passwordManager.createUser("Pepe", "hola12345678910111213141516171819202122");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),
+            "The username is already taken")]
+        public void createInvalidUserNameAlreadyTaken()
+        {
+            passwordManager.createUser("Pepe", "pepe1232");
+            passwordManager.createUser("Pepe", "121hola");
+        }
+
+    }
+}
