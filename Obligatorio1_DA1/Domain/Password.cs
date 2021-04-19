44277fccf02982f42a1c3f89d189be912bff6f6a
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-04-19 14:37:49 -0300

    Add password generation tests

--- Obligatorio1_DA1/Domain/Password.cs
+++ Obligatorio1_DA1/Domain/Password.cs
@@ -14,9 +14,46 @@ namespace Obligatorio1_DA1.Domain
         public string Pass { get; set; }
         public string Notes { get; set; }
 
-        public static string generate(int length, Boolean upercase, Boolean lowercase, Boolean digits, Boolean specialDigits)
+        public static string generate(int length, Boolean uppercase, Boolean lowercase, Boolean digits, Boolean specialDigits)
         {
-            return "hola";
+            List<char> validChars = new List<char>();
+            string pass = "";
+
+            if (uppercase)
+            {
+                List<char> uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToList();
+                addRandomChar(ref pass, uppercaseLetters);
+                validChars.AddRange(uppercaseLetters);
+            }
+            if (lowercase)
+            {
+                List<char> lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".ToList();
+                addRandomChar(ref pass, lowercaseLetters);
+                validChars.AddRange(lowercaseLetters);
+            }
+            if (digits)
+            {
+                List<char> digitList = "0123456789".ToList();
+                addRandomChar(ref pass, digitList);
+                validChars.AddRange(digitList);
+            }
+            if (specialDigits)
+            {
+                List<char> specialDigitList = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~".ToList();
+                addRandomChar(ref pass, specialDigitList);
+                validChars.AddRange(specialDigitList);
+            }
+            while (pass.Length < length)
+                addRandomChar(ref pass, validChars);
+
+            return pass;
+        }
+
+        private static void addRandomChar(ref string word, List<char> validChars)
+        {
+            Random random = new Random();
+            char randomChar = validChars[random.Next(0, validChars.Count - 1)];
+            word += randomChar;
         }
     }
 
--- UnitTestObligatorio1/UnitTestPassword.cs
+++ UnitTestObligatorio1/UnitTestPassword.cs
@@ -8,15 +8,8 @@ using System.Text.RegularExpressions;
 namespace UnitTestObligatorio1
 {
     [TestClass]
-    class UnitTestPassword
+    public class UnitTestPassword
     {
-        PasswordManager passwordManager;
-        [TestInitialize]
-        public void createPasswordManagerBeforeTests()
-        {
-            passwordManager = new PasswordManager();
-            passwordManager.createUser("Pepe", "Pepe$123");
-        }
 
         [TestMethod]
         public void createNewPassword()
@@ -39,16 +32,42 @@ namespace UnitTestObligatorio1
 
         }
 
-        [DataRow(12, false, true, false, false, "[a-z]{" + "12}+")]
-        [DataRow(12, false, true, false, false, "[a-z]{" + "34}+")]
-        [DataRow(12, false, true, false, false, "[a-z]{" + "12}+")]
+        [TestMethod]
+        public void createNewPasswordNoNotes()
+        {
+            try
+            {
+                Password pass = new Password
+                {
+                    Category = "Work",
+                    Site = "work.com",
+                    Username = "Joseph",
+                    Pass = "wwwjosph"
+                };
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+
+        }
+
+
+
+        [DataRow(1, true, false, false, false, "[A-Z]+")]
+        [DataRow(1, false, true, false, false, "[a-z]+")]
+        [DataRow(1, true, true, false, false, "^(?=.*[a-zA-Z]).{2}$")]
+        [DataRow(1, false, false, true, false, "[0-9]+")]
+        [DataRow(1, false, false, false, true, "([ -/]|[:-@]|[\\[-`]|[\\|-~])+")]
+        [DataRow(4, true, true, true, true, "[ -~]{4}")]
+        [DataRow(20, true, true, true, true, "[ -~]{20}")]
         [DataTestMethod]
         public void generateValidPassword
                (int length, Boolean upercase, Boolean lowercase, Boolean digits, Boolean specialDigits, string regex)
         {
             string pass = Password.generate(length, upercase, lowercase, digits, specialDigits);
             Regex r = new Regex(regex);
-            Assert.IsTrue(r.IsMatch(pass));
+            Assert.IsTrue(r.IsMatch(pass), "Password: " + pass + " Regex: " + regex);
         }
 
 
