599f01b64b4f8b7edc78d251773e4b6d3bd49a40
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-05-03 19:05:16 -0300

    Adss password Strenght user form

--- Interfaz/MainWindow.Designer.cs
+++ Interfaz/MainWindow.Designer.cs
@@ -29,6 +29,7 @@ namespace Presentation
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnlMainScreen = new System.Windows.Forms.Panel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,6 +38,9 @@ namespace Presentation
             this.tsmiCategories = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiDataBreaches = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiPasswordStrength = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.pnlMainScreen.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -44,19 +48,20 @@ namespace Presentation
             // pnlMainScreen
             // 
             this.pnlMainScreen.Controls.Add(this.menuStrip1);
-            this.pnlMainScreen.Location = new System.Drawing.Point(11, 11);
-            this.pnlMainScreen.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlMainScreen.Location = new System.Drawing.Point(15, 14);
+            this.pnlMainScreen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlMainScreen.Name = "pnlMainScreen";
-            this.pnlMainScreen.Size = new System.Drawing.Size(780, 399);
+            this.pnlMainScreen.Size = new System.Drawing.Size(1040, 491);
             this.pnlMainScreen.TabIndex = 2;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(780, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1040, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,52 +74,76 @@ namespace Presentation
             this.tsmiDataBreaches,
             this.tsmiPasswordStrength});
             this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
             this.menuToolStripMenuItem.Text = "Menu";
             // 
             // tsmiPasswords
             // 
             this.tsmiPasswords.Name = "tsmiPasswords";
-            this.tsmiPasswords.Size = new System.Drawing.Size(205, 22);
+            this.tsmiPasswords.Size = new System.Drawing.Size(258, 26);
             this.tsmiPasswords.Text = "Contraseñas";
             this.tsmiPasswords.Click += new System.EventHandler(this.tsmiPasswords_Click);
             // 
             // tsmiCreditCards
             // 
             this.tsmiCreditCards.Name = "tsmiCreditCards";
-            this.tsmiCreditCards.Size = new System.Drawing.Size(205, 22);
+            this.tsmiCreditCards.Size = new System.Drawing.Size(258, 26);
             this.tsmiCreditCards.Text = "Tarjetas";
             this.tsmiCreditCards.Click += new System.EventHandler(this.tsmiCreditCards_Click);
             // 
             // tsmiCategories
             // 
             this.tsmiCategories.Name = "tsmiCategories";
-            this.tsmiCategories.Size = new System.Drawing.Size(205, 22);
+            this.tsmiCategories.Size = new System.Drawing.Size(258, 26);
             this.tsmiCategories.Text = "Categorias";
             this.tsmiCategories.Click += new System.EventHandler(this.tsmiCategories_Click);
             // 
             // tsmiDataBreaches
             // 
             this.tsmiDataBreaches.Name = "tsmiDataBreaches";
-            this.tsmiDataBreaches.Size = new System.Drawing.Size(205, 22);
+            this.tsmiDataBreaches.Size = new System.Drawing.Size(258, 26);
             this.tsmiDataBreaches.Text = "Data Breach";
             this.tsmiDataBreaches.Click += new System.EventHandler(this.tsmiDataBreaches_Click);
             // 
             // tsmiPasswordStrength
             // 
             this.tsmiPasswordStrength.Name = "tsmiPasswordStrength";
-            this.tsmiPasswordStrength.Size = new System.Drawing.Size(205, 22);
+            this.tsmiPasswordStrength.Size = new System.Drawing.Size(258, 26);
             this.tsmiPasswordStrength.Text = "Fortaleza de Contraseñas";
             this.tsmiPasswordStrength.Click += new System.EventHandler(this.tsmiPasswordStrength_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(842, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1069, 24);
+            this.menuStrip2.TabIndex = 4;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 421);
+            this.ClientSize = new System.Drawing.Size(1069, 518);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.pnlMainScreen);
+            this.Controls.Add(this.menuStrip2);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainWindow";
             this.Text = "PasswordManager";
             this.pnlMainScreen.ResumeLayout(false);
@@ -122,6 +151,7 @@ namespace Presentation
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -134,5 +164,8 @@ namespace Presentation
         private System.Windows.Forms.ToolStripMenuItem tsmiCategories;
         private System.Windows.Forms.ToolStripMenuItem tsmiDataBreaches;
         private System.Windows.Forms.ToolStripMenuItem tsmiPasswordStrength;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
---
+++ Interfaz/PasswordStrength.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Presentation
+{
+    partial class PasswordStrength
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvReport = new System.Windows.Forms.DataGridView();
+            this.group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvReport);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(298, 399);
+            this.panel1.TabIndex = 0;
+            // 
+            // dgvReport
+            // 
+            this.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReport.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.group,
+            this.quantity});
+            this.dgvReport.Location = new System.Drawing.Point(6, 45);
+            this.dgvReport.Name = "dgvReport";
+            this.dgvReport.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvReport.Size = new System.Drawing.Size(292, 313);
+            this.dgvReport.TabIndex = 0;
+            // 
+            // group
+            // 
+            this.group.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.group.HeaderText = "Grupo";
+            this.group.Name = "group";
+            // 
+            // quantity
+            // 
+            this.quantity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.quantity.HeaderText = "Cantidad de Contraseñas";
+            this.quantity.Name = "quantity";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(304, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(476, 399);
+            this.panel2.TabIndex = 1;
+            // 
+            // PasswordStrength
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "PasswordStrength";
+            this.Size = new System.Drawing.Size(780, 399);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvReport;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn group;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantity;
+    }
+}
