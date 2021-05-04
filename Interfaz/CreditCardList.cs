ea872a5780b84f2e6d1e8efb7831e8a154480557
FranRossi <fr219401@fi365.ort.edu.uy>
2021-05-04 17:04:27 -0300

    Updates how to show credit cards numbers

--- Interfaz/CreditCardList.cs
+++ Interfaz/CreditCardList.cs
@@ -23,42 +23,6 @@ namespace Presentation
 
         private void FormatTblCreditCard()
         {
-            User _user = new User()
-            {
-                Name = "Gonzalo",
-                Pass = "HolaSoyGonzalo123"
-            };
-            Category _category = new Category()
-            {
-                Name = "Personal"
-            };
-            _user.Categories.Add(_category);
-            CreditCard _card1 = new CreditCard
-            {
-                User = _user,
-                Category = _category,
-                Name = "Visa Gold",
-                Type = "Visa",
-                Number = "2354678713003498",
-                SecureCode = "189",
-                ExpirationDate = "10/21",
-                Notes = "Límite 400k UYU"
-            };
-
-            CreditCard _card2 = new CreditCard
-            {
-                User = _user,
-                Category = _category,
-                Name = "Visa Gold",
-                Type = "Visa",
-                Number = "2354678713003450",
-                SecureCode = "189",
-                ExpirationDate = "07/24",
-                Notes = "Límite 400k UYU"
-            };
-
-            _myPasswordManager.CreateCreditCard(_card1);
-            _myPasswordManager.CreateCreditCard(_card2);
             List<CreditCard> creditCards = _myPasswordManager.GetCreditCards();
             tblCreditCards.DataSource = creditCards;
             foreach (DataGridViewColumn column in tblCreditCards.Columns)
@@ -74,7 +38,7 @@ namespace Presentation
                     case "Type":
                         column.HeaderText = "Tipo";
                         break;
-                    case "Number":
+                    case "SecretNumber":
                         column.HeaderText = "Numero";
                         break;
                     case "ExpirationDate":
