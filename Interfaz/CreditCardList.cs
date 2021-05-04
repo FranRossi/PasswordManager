e0d8115e53c4dc2590c94443270589106251f8ed
FranRossi <fr219401@fi365.ort.edu.uy>
2021-05-04 19:08:29 -0300

    Makes the delete credit card  buttonworks

--- Interfaz/CreditCardList.cs
+++ Interfaz/CreditCardList.cs
@@ -18,12 +18,52 @@ namespace Presentation
         {
             InitializeComponent();
             _myPasswordManager = pPasswordManager;
+            User _user = new User()
+            {
+                Name = "Gonzalo",
+                Pass = "HolaSoyGonzalo123"
+            };
+            Category _category = new Category()
+            {
+                Name = "Personal"
+            };
+            _user.Categories.Add(_category);
+            CreditCard _card1 = new CreditCard
+            {
+                User = _user,
+                Category = _category,
+                Name = "Visa Gold",
+                Type = "Visa",
+                Number = "2354678713003498",
+                SecureCode = "189",
+                ExpirationDate = "10/21",
+                Notes = "Límite 400k UYU"
+            };
+
+            CreditCard _card2 = new CreditCard
+            {
+                User = _user,
+                Category = _category,
+                Name = "Visa Black",
+                Type = "Visa",
+                Number = "2354678713001234",
+                SecureCode = "111",
+                ExpirationDate = "10/20",
+                Notes = "Límite 400k UYU"
+            };
+
+
+            _myPasswordManager.CreateCreditCard(_card1);
+            _myPasswordManager.CreateCreditCard(_card2);
             LoadTblCreditCard();
         }
 
         private void LoadTblCreditCard()
         {
+
             List<CreditCard> creditCards = _myPasswordManager.GetCreditCards();
+            tblCreditCards.DataSource = null;
+            tblCreditCards.Rows.Clear();
             tblCreditCards.DataSource = creditCards;
             foreach (DataGridViewColumn column in tblCreditCards.Columns)
             {
@@ -58,8 +98,9 @@ namespace Presentation
 
         private void btnDeleteCreditCard_Click(object sender, EventArgs e)
         {
-            var creditCardRow = tblCreditCards.CurrentRow.DataBoundItem;
-
+            CreditCard creditCardRow = (CreditCard)tblCreditCards.CurrentRow.DataBoundItem;
+            _myPasswordManager.DeleteCreditCard(creditCardRow);
+            LoadTblCreditCard();
         }
 
     }
