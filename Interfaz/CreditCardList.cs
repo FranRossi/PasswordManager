1f6f5c1b9a5e758db3cdb51b93f64bc0331e79c2
FranRossi <fr219401@fi365.ort.edu.uy>
2021-05-04 19:15:21 -0300

    Refactors method, now loads and format the table in two separeted methods

--- Interfaz/CreditCardList.cs
+++ Interfaz/CreditCardList.cs
@@ -18,53 +18,19 @@ namespace Presentation
         {
             InitializeComponent();
             _myPasswordManager = pPasswordManager;
-            User _user = new User()
-            {
-                Name = "Gonzalo",
-                Pass = "HolaSoyGonzalo123"
-            };
-            Category _category = new Category()
-            {
-                Name = "Personal"
-            };
-            _user.Categories.Add(_category);
-            CreditCard _card1 = new CreditCard
-            {
-                User = _user,
-                Category = _category,
-                Name = "Visa Gold",
-                Type = "Visa",
-                Number = "2354678713003498",
-                SecureCode = "189",
-                ExpirationDate = "10/21",
-                Notes = "Límite 400k UYU"
-            };
-
-            CreditCard _card2 = new CreditCard
-            {
-                User = _user,
-                Category = _category,
-                Name = "Visa Black",
-                Type = "Visa",
-                Number = "2354678713001234",
-                SecureCode = "111",
-                ExpirationDate = "10/20",
-                Notes = "Límite 400k UYU"
-            };
-
-
-            _myPasswordManager.CreateCreditCard(_card1);
-            _myPasswordManager.CreateCreditCard(_card2);
             LoadTblCreditCard();
         }
 
         private void LoadTblCreditCard()
         {
-
             List<CreditCard> creditCards = _myPasswordManager.GetCreditCards();
             tblCreditCards.DataSource = null;
             tblCreditCards.Rows.Clear();
             tblCreditCards.DataSource = creditCards;
+            FormatCreditCardListOnTable(creditCards);
+        }
+        private void FormatCreditCardListOnTable(List<CreditCard> creditCards)
+        {
             foreach (DataGridViewColumn column in tblCreditCards.Columns)
             {
                 switch (column.Name)
@@ -90,6 +56,7 @@ namespace Presentation
                 }
             }
         }
+
 
         private void btnAddsCreditCard_Click(object sender, EventArgs e)
         {
