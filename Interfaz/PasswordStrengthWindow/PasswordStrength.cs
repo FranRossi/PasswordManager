90b133393f050c35615e7730581278a2c478193d
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-05-05 18:35:55 -0300

    Improves layout

---
+++ Interfaz/PasswordStrengthWindow/PasswordStrength.cs
@@ -0,0 +1,61 @@
+﻿using Obligatorio1_DA1.Domain;
+using Obligatorio1_DA1.Utilities;
+using Presentation.PasswordStrengthWindow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation
+{
+    public partial class PasswordStrength : UserControl
+    {
+        private PasswordManager _passwordManager;
+        private PasswordStrengthChart chartPanel;
+        public PasswordStrength(PasswordManager passwordManager)
+        {
+            this._passwordManager = passwordManager;
+            InitializeComponent();
+            this.ShowChart();
+            this.SetColorQuantities();
+        }
+
+        private void SetColorQuantities()
+        {
+            Dictionary<PasswordStrengthColor, Label> labelOfColor = new Dictionary<PasswordStrengthColor, Label>();
+            labelOfColor.Add(PasswordStrengthColor.DarkGreen, lblDarkGreenQuantity);
+            labelOfColor.Add(PasswordStrengthColor.LightGreen, lblLightGreenQuantity);
+            labelOfColor.Add(PasswordStrengthColor.Yellow, lblYellowQuantity);
+            labelOfColor.Add(PasswordStrengthColor.Orange, lblOrangeQuantity);
+            labelOfColor.Add(PasswordStrengthColor.Red, lblRedQuantity);
+
+
+            List<passwordReportByColor> report = this._passwordManager.GetPasswordReportByColor(null);
+            foreach (passwordReportByColor entry in report)
+            {
+                Label lbl = labelOfColor[entry.Color];
+                lbl.Text = entry.Quantity.ToString();
+            }
+        }
+
+        private void ShowChart()
+        {
+            if (this.chartPanel == null)
+            {
+                this.chartPanel = new PasswordStrengthChart(new List<Obligatorio1_DA1.Utilities.passwordReportByCategoryAndColor>());
+            }
+            pnlChartList.Controls.Clear();
+            pnlChartList.Controls.Add(chartPanel);
+        }
+
+        private void btmShowChart_Click(object sender, EventArgs e)
+        {
+            this.ShowChart();
+        }
+    }
+}
--- Interfaz/PasswordStrengthWindow/PasswordStrengthChart.cs
+++ Interfaz/PasswordStrengthWindow/PasswordStrengthChart.cs
@@ -1,4 +1,4 @@
-﻿using Obligatorio1_DA1.Domain;
+﻿using Obligatorio1_DA1.Utilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,30 +9,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Presentation
+namespace Presentation.PasswordStrengthWindow
 {
-    public partial class PasswordStrength : UserControl
+    public partial class PasswordStrengthChart : UserControl
     {
-        private PasswordManager _passwordManager;
-        public PasswordStrength(PasswordManager passwordManager)
+        public PasswordStrengthChart(List<passwordReportByCategoryAndColor> chartData)
         {
-            this._passwordManager = passwordManager;
             InitializeComponent();
-            this.LoadChart();
-
-            /*            tbgroup.width = tbgroup.parent.width;
-                        tbgroup.height = 300;
-                        tbgroup.location = new point(0, tbgroup.location.y);
-                        tbgroup.anchor = anchorstyles.left | anchorstyles.right;*/
+            LoadChart(chartData);
         }
 
-
-        private void tbGroup_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void LoadChart()
+        private void LoadChart(List<passwordReportByCategoryAndColor> chartData)
         {
             List<string> categories = new List<string> { "Persona", "Trabajo", "Facultad" };
             List<string> colors = new List<string> { "Rojo", "Amarillo", "Verde" };
