fac96446dcb6d0e60d6c1af040be2df4bdb3b115
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-05-08 18:48:26 -0300

    adds interface and concrete class

---
+++ Obligatorio1_DA1/Utilities/DataBreachFromString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1_DA1.Utilities
+{
+    class DataBreachFromString : IDataBreach<string>
+    {
+        public string ConvertToDataBreachString(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
