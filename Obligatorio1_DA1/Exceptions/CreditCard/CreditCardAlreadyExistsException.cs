ce26516cafb2ea7d7fc37f7e537bb4ff8e09ada8
FranRossi <fr219401@fi365.ort.edu.uy>
2021-05-08 17:08:59 -0300

    Creates exception file for credit card

---
+++ Obligatorio1_DA1/Exceptions/CreditCard/CreditCardAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿namespace Obligatorio1_DA1.Exceptions
+{
+
+    public class CreditCardAlreadyExistsException : ValidationException
+    {
+        public CreditCardAlreadyExistsException()
+        {
+            this.message = "La tarjeta de crédito/débito ya existe en el sistema.";
+        }
+    }
+}
