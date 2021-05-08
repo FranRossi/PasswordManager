0e1b8cfa7fe6314dc920c25a2e140c537ab62a6c
Agustin Ferrari Luciano <40501091+agustinferrari@users.noreply.github.com>
2021-05-08 14:23:14 -0300

    deletes regex form names

--- Obligatorio1_DA1/Domain/Password.cs
+++ Obligatorio1_DA1/Domain/Password.cs
@@ -190,38 +190,38 @@ namespace Obligatorio1_DA1.Domain
 
         private bool IsRedStrength(string pass)
         {
-            Regex lengthBetween1And8Regex = new Regex(@"^.{1,8}$", RegexOptions.Compiled);
-            return lengthBetween1And8Regex.IsMatch(pass);
+            Regex lengthBetween1And8 = new Regex(@"^.{1,8}$", RegexOptions.Compiled);
+            return lengthBetween1And8.IsMatch(pass);
         }
 
         private bool IsOrangeStrength(string pass)
         {
-            Regex lengthBetween8And14Regex = new Regex(@"^.{8,14}$", RegexOptions.Compiled);
-            return lengthBetween8And14Regex.IsMatch(pass);
+            Regex lengthBetween8And14 = new Regex(@"^.{8,14}$", RegexOptions.Compiled);
+            return lengthBetween8And14.IsMatch(pass);
         }
 
         private bool IsSymbol(char character)
         {
-            Regex isSymbolRegex = new Regex(@"^[ -/:-@[-`{-~]+$", RegexOptions.Compiled);
-            return isSymbolRegex.IsMatch(character.ToString());
+            Regex isSymbol = new Regex(@"^[ -/:-@[-`{-~]+$", RegexOptions.Compiled);
+            return isSymbol.IsMatch(character.ToString());
         }
 
         private bool IsNumber(char character)
         {
-            Regex isNumberRegex = new Regex(@"^[0-9]+$", RegexOptions.Compiled);
-            return isNumberRegex.IsMatch(character.ToString());
+            Regex isNumber = new Regex(@"^[0-9]+$", RegexOptions.Compiled);
+            return isNumber.IsMatch(character.ToString());
         }
 
         private bool IsUpperCase(char character)
         {
-            Regex isUpperCaseRegex = new Regex(@"^[A-Z]+$", RegexOptions.Compiled);
-            return isUpperCaseRegex.IsMatch(character.ToString());
+            Regex isUpperCase = new Regex(@"^[A-Z]+$", RegexOptions.Compiled);
+            return isUpperCase.IsMatch(character.ToString());
         }
 
         private bool IsLowerCase(char character)
         {
-            Regex isLowerCaseRegex = new Regex(@"^[a-z]+$", RegexOptions.Compiled);
-            return isLowerCaseRegex.IsMatch(character.ToString());
+            Regex isLowerCase = new Regex(@"^[a-z]+$", RegexOptions.Compiled);
+            return isLowerCase.IsMatch(character.ToString());
         }
 
         public void ShareWithUser(User userShareWith)
