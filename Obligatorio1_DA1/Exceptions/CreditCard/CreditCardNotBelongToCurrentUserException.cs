74376eb2835ddfc7639edb6683bda6be7e2e2af5
FranRossi <fr219401@fi365.ort.edu.uy>
2021-05-08 17:22:24 -0300

    Creates credit card excpetion

---
+++ Obligatorio1_DA1/Exceptions/CreditCard/CreditCardNotBelongToCurrentUserException.cs
@@ -0,0 +1,11 @@
+﻿namespace Obligatorio1_DA1.Exceptions
+{
+
+    public class CreditCardNotBelongToCurrentUserException : ValidationException
+    {
+        public CreditCardNotBelongToCurrentUserException()
+        {
+            this.message = "Esta tarjeta de crédito/débito no pertenece al Usuario que esta logeado";
+        }
+    }
+}
