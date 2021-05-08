abfd32ad59dd5341993bcbb6ca03f68d8ed53782
FranRossi <fr219401@fi365.ort.edu.uy>
2021-05-08 17:05:59 -0300

    Creates test to modify credit card that already exist

--- UnitTestObligatorio1/UnitTestCreditCard.cs
+++ UnitTestObligatorio1/UnitTestCreditCard.cs
@@ -288,6 +288,37 @@ namespace UnitTestObligatorio1
             CollectionAssert.Contains(creditCards, _card2);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(CreditCardAlreadyExistsException))]
+        public void ModifyCreditCardThatAlreadyExists()
+        {
+            CreditCard creditCardAlreadyInPasswordManager = new CreditCard
+            {
+                User = _passwordManager.CurrentUser,
+                Category = this._category,
+                Name = "MasterCard Black",
+                Type = "Master",
+                Number = "2354678713001111",
+                SecureCode = "111",
+                ExpirationDate = "02/30",
+                Notes = "Límite 400 shenn UYU"
+            };
+            _passwordManager.CreateCreditCard(creditCardAlreadyInPasswordManager);
+
+            CreditCard newCreditCard = new CreditCard
+            {
+                User = _passwordManager.CurrentUser,
+                Category = this._category,
+                Name = "MasterCard Black",
+                Type = "Master",
+                Number = "2354678713001111",
+                SecureCode = "111",
+                ExpirationDate = "02/30",
+                Notes = "Límite 400 shenn UYU"
+            };
+            this._passwordManager.ModifyPasswordOnCurrentUser(this._card, newCreditCard);
+        }
+
 
 
     }
