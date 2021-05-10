ad1774623794ff939aab7804809695e2c6d5f1cc
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-05-10 14:47:47 -0300

    adds user not equal test

--- UnitTestObligatorio1/UnitTestSignUp.cs
+++ UnitTestObligatorio1/UnitTestSignUp.cs
@@ -76,7 +76,7 @@ namespace UnitTestObligatorio1
 
         [TestMethod]
         [ExpectedException(typeof(UserNameTooShortException))]
-        public void createUserEmptyName()
+        public void CreateUserEmptyName()
         {
             User newUser = new User("", "password");
             passwordManager.CreateUser(newUser);
@@ -84,7 +84,7 @@ namespace UnitTestObligatorio1
 
         [TestMethod]
         [ExpectedException(typeof(UserNameTooShortException))]
-        public void createUserNameTooShort()
+        public void CreateUserNameTooShort()
         {
             User newUser = new User("Khea", " ");
             passwordManager.CreateUser(newUser);
@@ -92,7 +92,7 @@ namespace UnitTestObligatorio1
 
         [TestMethod]
         [ExpectedException(typeof(UserNameTooLongException))]
-        public void createUserNameTooLong()
+        public void CreateUserNameTooLong()
         {
             User newUser = new User("MaritoBaracus1234VisualStudioEnterprise", "password");
             passwordManager.CreateUser(newUser);
@@ -109,6 +109,16 @@ namespace UnitTestObligatorio1
             string actualName = newUser.ToString();
             string expectedName = name;
             StringAssert.Equals(expectedName, actualName);
+        }
+
+        [TestMethod]
+        public void UserNotEqual()
+        {
+            User newUser1 = new User("Juancito", "hola123");
+
+            User newUser2 = new User("juancito", "hola123");
+
+            Assert.IsFalse(newUser1.Equals(newUser2));
         }
 
     }
