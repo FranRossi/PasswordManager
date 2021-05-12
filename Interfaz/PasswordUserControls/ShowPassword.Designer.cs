cd690ceeb3d26cc569a3b69d4adbdd9ac9db2e45
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-05-12 16:35:40 -0300

    Changes window location to center

--- Interfaz/PasswordUserControls/ShowPassword.Designer.cs
+++ Interfaz/PasswordUserControls/ShowPassword.Designer.cs
@@ -225,6 +225,7 @@ namespace Presentation.PasswordUserControls
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "ShowPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PasswordManager";
             this.pnlPassword.ResumeLayout(false);
             this.pnlPassword.PerformLayout();
