95badf247c9fe933749eae5ee829b919a10a0a27
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-01 19:07:26 -0300

    Adds user to Repository

---
+++ Repository/PasswordManagerDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Obligatorio1_DA1.Domain;
+
+namespace Repository
+{
+    class PasswordManagerDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public PasswordManagerDBContext() : base("name=PasswordManagerDB") { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserTypeConfiguration());
+        }
+    }
+}
---
+++ Repository/UserTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Obligatorio1_DA1.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserTypeConfiguration : EntityTypeConfiguration<User>
+    {
+        public UserTypeConfiguration()
+        {
+            this.HasKey(user => user.Name);
+            this.Property(user => user.Name).HasMaxLength(25);
+            this.Property(user => user.MasterPass).HasMaxLength(25);
+            this.Property(user => user.MasterPass).IsRequired();
+        }
+    }
+}
