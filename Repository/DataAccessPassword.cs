b61b772a65e3fb4ac023adf0ccfc4d0dbb0d313c
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-03 19:47:20 -0300

    Adds add password

---
+++ Repository/DataAccessPassword.cs
@@ -0,0 +1,26 @@
+﻿using Obligatorio1_DA1.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class DataAccessPassword : IDataAccess<Password>
+    {
+        public void Add(Password pPassword)
+        {
+            using (PasswordManagerDBContext context = new PasswordManagerDBContext())
+            {
+                context.Categories.Attach(pPassword.Category);
+                context.Users.Attach(pPassword.User);
+                context.Passwords.Add(pPassword);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
