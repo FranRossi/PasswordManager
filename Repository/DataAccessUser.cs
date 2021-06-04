c63ef2e97796a2444199e5cdd3c53c7723f8acf5
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-06-03 21:30:37 -0300

    Removes unnecessary methode from data acces User

--- Repository/DataAccessUser.cs
+++ Repository/DataAccessUser.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Repository
 {
-    public class DataAccessUser : IDataAccess<User>
+    public class DataAccessUser
     {
         public void Add(User pUser)
         {
@@ -16,27 +16,6 @@ namespace Repository
                 context.Users.Attach(pUser);
                 context.SaveChanges();
             }
-        }
-
-        public void Delete(User pUser)
-        {
-            using (PasswordManagerDBContext context = new PasswordManagerDBContext())
-            {
-                User userToDelete = context.Users.FirstOrDefault(u => u.MasterName == pUser.MasterName);
-                context.Users.Remove(userToDelete);
-                context.SaveChanges();
-            }
-        }
-
-        public IEnumerable<User> GetAll()
-        {
-            throw new NotImplementedException(); // TODO delete methode
-        }
-
-        public void Modify(User pUser)
-        {
-            throw new NotImplementedException(); // TODO delete methode
-
         }
     }
 }
