47b7f817d7760ed8641bc89292b7742f9e30ab9f
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-06-07 18:49:42 -0300

    Creates test for hash algorithm

---
+++ UnitTestObligatorio1/Hash/UnitTestHash.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obligatorio1_DA1.Domain;
+using Obligatorio1_DA1.Utilities;
+using Repository;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestObligatorio1
+{
+
+    [TestClass]
+    public abstract class UnitTestHash
+    {
+        protected abstract IHash GetHash();
+        protected IEncription encription;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            try
+            {
+                encription = GetEncription();
+
+            }
+            catch (Exception exception)
+            {
+                Assert.Fail("Expected no exception, but got: " + exception.Message);
+            }
+
+        }
+
+        [DataRow("mySuperSecurePassword", "keyy")]
+        [DataRow("12321pass werod", "keyadsfy")]
+        [DataRow("hello world", "asdfasd")]
+        [DataTestMethod]
+        public void EncriptDifferentThanOrignal(string textToEncript, string key)
+        {
+            string encriptedText = encription.Encript(textToEncript, key);
+            Assert.AreNotEqual(textToEncript, encriptedText, "Original: " + textToEncript + " Result: " + encriptedText);
+        }
+
+
+        [DataRow("mySuperSecurePassword", "keyy")]
+        [DataRow("12321pass werod", "keyadsfy")]
+        [DataRow("hello world", "asdfasd")]
+        [DataTestMethod]
+        public void Decript(string textToEncript, string key)
+        {
+            string encriptedText = encription.Encript(textToEncript, key);
+            string decriptedText = encription.Decript(encriptedText, key);
+            Assert.AreEqual(textToEncript, decriptedText, "Original: " + textToEncript + " Result: " + decriptedText);
+        }
+
+    }
+}
