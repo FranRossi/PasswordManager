03209014d050ebc48d9093e0104c3a3cfb4a34d3
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-06-07 19:43:16 -0300

    Adds unit test for encription

---
+++ UnitTestObligatorio1/UnitTestPasswordEncription.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obligatorio1_DA1.Domain;
+using BusinessLogic;
+using Obligatorio1_DA1.Exceptions;
+using System;
+using System.Collections.Generic;
+using Repository;
+
+namespace UnitTestObligatorio1
+{
+    [TestClass]
+    public class UnitTestPasswordEncription
+    {
+        private Password _password;
+        private PasswordManager _passwordManager;
+        private User _user;
+        private Category _category;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            using (PasswordManagerDBContext context = new PasswordManagerDBContext())
+            {
+                context.Database.ExecuteSqlCommand("DELETE FROM PASSWORDS");
+                context.Database.ExecuteSqlCommand("DELETE FROM CREDITCARDS");
+                context.Database.ExecuteSqlCommand("DELETE FROM USERS");
+            }
+            try
+            {
+                _passwordManager = new PasswordManager();
+                _user = new User()
+                {
+                    MasterName = "Gonzalo",
+                    MasterPass = "HolaSoyGonzalo123"
+                };
+                _category = new Category()
+                {
+                    Name = "Personal"
+                };
+                _passwordManager.CreateUser(_user);
+                _passwordManager.CreateCategoryOnCurrentUser(_category.Name);
+                _category = _passwordManager.GetCategoriesFromCurrentUser().ToArray()[0];
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            using (PasswordManagerDBContext context = new PasswordManagerDBContext())
+            {
+                context.Database.ExecuteSqlCommand("DELETE FROM PASSWORDS");
+                context.Database.ExecuteSqlCommand("DELETE FROM CREDITCARDS");
+                context.Database.ExecuteSqlCommand("DELETE FROM USERS");
+            }
+        }
+
+        [DataRow("mySuperSecurePassword")]
+        [DataRow("12321pass werod")]
+        [DataRow("hello world")]
+        [DataTestMethod]
+        public void EncriptedPasswordDifferentFromOriginal(string passName)
+        {
+            _password = new Password
+            {
+                User = _user,
+                Category = _category,
+                Site = "ort.edu.uy",
+                Username = "239850",
+                Pass = passName,
+                Notes = "No me roben la cuenta"
+            };
+            _passwordManager.CreatePassword(_password);
+
+            List<Password> userPasswords = _passwordManager.GetPasswords();
+            CollectionAssert.Contains(userPasswords, _password);
+        }
+
+    }
+}
