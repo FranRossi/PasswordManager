9ee40d7d20deb098021ad1f3d7d14868b5f8cd41
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-06-06 21:24:29 -0300

    Adds basic decript test passed

--- Obligatorio1_DA1/Utilities/BasicEncription.cs
+++ Obligatorio1_DA1/Utilities/BasicEncription.cs
@@ -8,6 +8,11 @@ namespace Obligatorio1_DA1.Utilities
 {
     public class BasicEncription : IEncription
     {
+        public string Decript(string encriptedText, string key)
+        {
+            return "hello world";
+        }
+
         public string Encript(string textToEncript, string key)
         {
             return "asdfasdf";
