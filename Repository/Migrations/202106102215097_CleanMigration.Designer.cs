37fcf963f04d87a92c572b7abf3df6bc4ca0b09f
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-06-10 19:22:34 -0300

    Fixes merge conflict, problems with migration and decryption

--- Repository/Migrations/202106102215097_CleanMigration.Designer.cs
+++ Repository/Migrations/202106102215097_CleanMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace Repository.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class EncryptedPass : IMigrationMetadata
+    public sealed partial class CleanMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(EncryptedPass));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(CleanMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "202106101522467_EncryptedPass"; }
+            get { return "202106102215097_CleanMigration"; }
         }
         
         string IMigrationMetadata.Source
