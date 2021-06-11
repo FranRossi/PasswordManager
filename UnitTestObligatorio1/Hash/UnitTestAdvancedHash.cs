9538e6777c4005573c6b89a0633e4457c36bf1eb
IvanMonjardin <IM239850@fi365.ort.edu.uy>
2021-06-11 17:52:21 -0300

    Adds tests for advancedHash

---
+++ UnitTestObligatorio1/Hash/UnitTestAdvancedHash.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obligatorio1_DA1.Domain;
+using Obligatorio1_DA1.Utilities;
+using Repository;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestObligatorio1
+{
+
+    [TestClass]
+    public class UnitTestAdvancedHash : UnitTestHash
+    {
+        protected override IHash GetHash()
+        {
+            AdvancedHash basicHash = new AdvancedHash();
+            return basicHash;
+        }
+    }
+}
