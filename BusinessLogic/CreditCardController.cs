2f7ffe9c620229bac5285ac23737bf3330df1721
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-12 17:19:28 -0300

    Adds CreditCardController

---
+++ BusinessLogic/CreditCardController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Repository;
+using System.Collections.Generic;
+using Obligatorio1_DA1.Utilities;
+using Obligatorio1_DA1.Exceptions;
+using Obligatorio1_DA1.Domain;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public class CreditCardController
+    {
+        private SessionController _sessionController;
+        private CategoryController _categoryController;
+        private IDataAccessCreditCard<CreditCard> _creditCards;
+
+        public CreditCardController()
+        {
+            _creditCards = new DataAccessCreditCard();
+            _sessionController = SessionController.GetInstance();
+            _categoryController = new CategoryController();
+        }
+
+        //----------------------------------------------------CREDIT CARD CONTROLLER---------------------------------------
+        public void CreateCreditCard(CreditCard newCreditCard)
+        {
+            VerifyCreditCardBelongToCurrentUser(newCreditCard);
+            VerifyCreditCardUniqueness(newCreditCard);
+            _categoryController.VerifyItemCategoryBelongsToUser(newCreditCard);
+            _creditCards.Add(newCreditCard);
+        }
+
+        private void VerifyCreditCardUniqueness(CreditCard newCreditCard)
+        {
+            if (!_creditCards.CheckUniqueness(newCreditCard))
+                throw new CreditCardAlreadyExistsException();
+        }
+
+        public List<CreditCard> GetCreditCards()
+        {
+            string currentUserMasterName = _sessionController.GetCurrentUserMasterName();
+            return _creditCards.GetAll(currentUserMasterName).ToList();
+        }
+
+        public void ModifyCreditCardOnCurrentUser(CreditCard newCreditCard)
+        {
+            VerifyCreditCardBelongToCurrentUser(newCreditCard);
+            VerifyCreditCardUniqueness(newCreditCard);
+            _categoryController.VerifyItemCategoryBelongsToUser(newCreditCard);
+            _creditCards.Modify(newCreditCard);
+        }
+
+        public void DeleteCreditCard(CreditCard card)
+        {
+            _creditCards.Delete(card);
+        }
+
+        private void VerifyCreditCardBelongToCurrentUser(CreditCard newCreditCard)
+        {
+            if (!(newCreditCard.User.Equals(_sessionController.CurrentUser)))
+                throw new CreditCardNotBelongToCurrentUserException();
+        }
+    }
+}
