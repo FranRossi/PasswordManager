58d627bad4bada767f3c9253a8657c09d1c0ffaf
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-13 19:44:16 -0300

    Adds category controller interface

---
+++ BusinessLogic/ICategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Repository;
+using System.Collections.Generic;
+using Obligatorio1_DA1.Utilities;
+using Obligatorio1_DA1.Exceptions;
+using Obligatorio1_DA1.Domain;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public interface ICategoryController
+    {
+        List<Category> GetCategoriesFromCurrentUser();
+
+        void CreateCategoryOnCurrentUser(string category);
+
+        void VerifyCategoryUniqueness(Category newCategory);
+
+        void ModifyCategoryOnCurrentUser(Category modifiedCategory);
+
+        void VerifyItemCategoryBelongsToUser(Item newItem);
+    }
+}
