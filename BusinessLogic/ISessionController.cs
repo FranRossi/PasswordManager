a5cb583d97f26cff1fef75f3e8678a8ba1b292ca
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-13 20:04:59 -0300

    Adds session controller interface

---
+++ BusinessLogic/ISessionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Repository;
+using System.Collections.Generic;
+using Obligatorio1_DA1.Utilities;
+using Obligatorio1_DA1.Exceptions;
+using Obligatorio1_DA1.Domain;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public interface ISessionController
+    {
+        User CurrentUser { get; }
+
+        void CreateUser(User newUser);
+
+        void Login(string name, string password);
+
+        string GetCurrentUserMasterName();
+    }
+}
