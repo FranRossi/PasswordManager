01d9b74c88505bfd267caca620d08f676291e24a
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-13 20:01:32 -0300

    Adds password controller interface

---
+++ BusinessLogic/IPasswordController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Repository;
+using System.Collections.Generic;
+using Obligatorio1_DA1.Utilities;
+using Obligatorio1_DA1.Exceptions;
+using Obligatorio1_DA1.Domain;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public interface IPasswordController
+    {
+        void CreatePassword(Password newPassword);
+
+        void VerifyPassword(Password passwordToCheck);
+
+        List<Password> GetPasswords();
+
+        void DeletePassword(Password password);
+
+        void ModifyPasswordOnCurrentUser(Password newPassword);
+
+        public bool PasswordTextIsDuplicate(Password password);
+    }
+}
