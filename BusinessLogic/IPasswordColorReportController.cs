0f78edbd0cc349bc775a794af155975983eef05d
Agustin Ferrari <agustin_ferrari@hotmail.com>
2021-06-13 19:53:28 -0300

    Adds password color report controller interface

---
+++ BusinessLogic/IPasswordColorReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Repository;
+using System.Collections.Generic;
+using Obligatorio1_DA1.Utilities;
+using Obligatorio1_DA1.Exceptions;
+using Obligatorio1_DA1.Domain;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public interface IPasswordColorReportController
+    {
+        List<PasswordReportByCategoryAndColor> GetPasswordReportByCategoryAndColor();
+
+        List<PasswordReportByColor> GetPasswordReportByColor();
+
+        List<Password> GetPasswordsByColor(PasswordStrengthColor color);
+
+        bool PasswordIsNotGreenSecure(Password password);
+
+    }
+}
